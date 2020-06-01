818feb82a143c01631af2ce85a824d6f83588670
zhailzh <zlzdbf@gmail.com>
2020-06-01 11:53:45 +0800

    maven

--- src/main/java/L14.java
+++ src/main/java/L14.java
@@ -1,7 +1,11 @@
+import  org.apache.isis.core.commons.ensure.Assert;
+
 public class L14 {
 
     public static void main(String[] args) {
+        Assert.assertEquals("dd","dd");
         System.out.println("Hello,world");
+        
     }
 
 }
