0f85a095e343fed84430f2adbf8c1af08046100b
zhailzh <zlzdbf@gmail.com>
2020-07-17 20:25:26 +0800

    modify

--- src/main/java/LBig/L1034.java
+++ src/main/java/LBig/L1034.java
@@ -4,17 +4,19 @@ import java.util.Arrays;
 
 public class L1034 {
     public static void main(String[] args) {
-        System.out.println(numMovesStones(1,4,3));
+
+        System.out.println(Arrays.toString(numMovesStones(1, 4, 3)));
     }
-    public static  int[] numMovesStones(int a, int b, int c) {
-        int[] m = new int[]{a,b,c};
+
+    public static int[] numMovesStones(int a, int b, int c) {
+        int[] m = new int[]{a, b, c};
         Arrays.sort(m);
-        int left = m[1]-m[0]-1;
-        int right = m[2]-m[1]-1;
-        int first=0,seconde=0;
-        seconde=left+right;
-        first= Math.min(right==1||left==1?1:2,right==0?0:1+left==0?0:1);
-        return new int[]{first,seconde};
-        
+        int left = m[1] - m[0] - 1;
+        int right = m[2] - m[1] - 1;
+        int first = 0, seconde = 0;
+        seconde = left + right;
+        first = Math.min(right == 1 || left == 1 ? 1 : 2, (right == 0 ? 0 : 1) + (left == 0 ? 0 : 1));
+        return new int[]{first, seconde};
+
     }
 }
